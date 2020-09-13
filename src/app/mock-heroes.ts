c97d742ea4fd2316903a333b6f4fd2fa1b3039f4
Kyle Bentley <bajanbentley@users.noreply.github.com>
2020-09-13 15:52:15 -0400

    2. Display a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+    { id: 1, name: "Bussa" },
+    { id: 2, name: "Sarah Ann Gill" },
+    { id: 3, name: "Samuel Jackman Prescod" },
+    { id: 4, name: "Charles Duncan O'Neal" },
+    { id: 5, name: "Sir Grantley Herbert Adams" },
+    { id: 6, name: "Clement Osbourne Payne" },
+    { id: 7, name: "Sir Hugh Worrell Springer" },
+    { id: 8, name: "Sir Frank Leslie Walcott" },
+    { id: 9, name: "Errol Barrow" },
+    { id: 10, name: "Sir Garfield St. Aubrun Sobers" }
+]
